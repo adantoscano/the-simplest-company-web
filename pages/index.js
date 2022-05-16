2c1f224b42faadcb2c8a26a41a38f1f4eb5b1fc1
Adan Toscano <adantoscano@gmail.com>
2022-05-17 01:38:48 +0200

    feat: projects page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -19,6 +20,10 @@ export default function Home() {
         <p className={styles.description}>
           Simple does not mean easy
         </p>
+
+        <Link href="/projects">
+          Projects
+        </Link>
       </main>
     </div>
   )
